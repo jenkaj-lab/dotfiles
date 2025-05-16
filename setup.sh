dd0b88143634f9c189ba97c5cd235719a6e07850
alex <jenkins.alexanderj@gmail.com>
2025-05-16 17:23:24 -0100

    setup and readme

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Overwrite existing dotfiles
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.config ~/.config
+
+# Download software/packages
+sudo pacman -S --needed \
+	nvim \
+	kitty \
