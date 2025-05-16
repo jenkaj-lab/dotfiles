4f016929482b90f209314941abbbc200dc336c47
Alex <125412758+jenkaj-lab@users.noreply.github.com>
2025-05-16 22:29:10 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 # Download software/packages
-# Tiling wayland compositor: hyprland
-# Hyprland fix: xdg-desktop-portal-hyprland
-# Text editor: nvim
-# Terminal: kitty
-# Browser: chromium
-sudo pacman -S --needed hyprland
-sudo pacman -S --needed xdg-desktop-portal-hyprland 
-sudo pacman -S --needed nvim 
-sudo pacman -S --needed	kitty
-sudo pacman -S --needed chromium
+packages=(
+  hyprland  # tiling Wayland compositor
+  xdg-desktop-portal-hyprland  # needed for hyprland functionality
+  kitty  # terminal emulator
+  chromium  # web browser
+  nvim  # text editor
+)
+
+sudo pacman -S --needed --noconfirm "${packages[@]}"
 
 # Overwrite existing dotfiles
 cp -f ~/dotfiles/.bashrc ~/.bashrc
