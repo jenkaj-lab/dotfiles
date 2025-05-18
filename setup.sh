b0adb060d50b99b244ea8339b0bc9d612ad71727
alex <jenkins.alexanderj@gmail.com>
2025-05-17 23:13:13 -0100

    updates

--- setup.sh
+++ setup.sh
@@ -1,29 +1,61 @@
 #!/bin/bash
 
 # Download software/packages
+sudo echo "[*] Downloading software/packages"
 packages=(
-  hyprland  # tiling Wayland compositor
-  xdg-desktop-portal-hyprland  # needed for hyprland functionality
-  kitty  # terminal emulator
-  chromium  # web browser
-  nvim  # text editor
-  wofi # app launcher
-  yay # AUR helper
+  hyprland
+  xdg-desktop-portal-hyprland
+  kitty
+  chromium
+  nvim
+  wofi
+  # yay
 )
-
 sudo pacman -S --needed --noconfirm "${packages[@]}"
 
-# Replace existing dotfiles
-cp ~/dotfiles/.bashrc ~/.bashrc
-cp ~/dotfiles/.bash_profile ~/.bash_profile
-cp -r ~/dotfiles/.config ~/.config
+# Backup dotfiles and create symbolic links to new configs
+echo "[*] Installing dotfiles"
+dotfiles=(
+  ".bashrc"
+  ".bash_profile"
+  ".config"
+)
+for file in "${dotfiles[@]}"; do
+  if [ -e ~/"$file" ]; then
+    if [ ! -e ~/"$file~" ]; then
+      # create a backup for a dotfile if a backup doesn't already exist
+      echo "backing up $file"
+      mv ~/"$file" ~/"$file~"
+    fi
+  fi
+
+  # this line is purposely outside of the loop because it makes sure that symlinks are 
+  # still created even if the files/directories don't exist in the user's home directory
+  echo "installing $file"
+  ln -sfn "$(pwd)/$file" ~/"$file"
+done
 
 # Use the new configs
 source ~/.bashrc
 
 # Install the nvim packer
-#git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-# ~/.local/share/nvim/site/pack/packer/opt/packer.nvim
+packer_repo=https://github.com/wbthomason/packer.nvim
+packer_dir=~/.local/share/nvim/site/pack/packer/opt/packer.nvim
+if [ ! -e $packer_dir ]; then
+  echo "[*] Downloading packer for nvim"
+  git clone --depth 1 "$packer_repo" "$packer_dir"
+fi
+
+# NOTE: Ignore the warning, it's a deprecated package
+# Ensure packer is loaded before using it
+export PACKER_DIR="$packer_dir"
+
+# Run PackerCompile and PackerInstall instead of PackerSync
+nvim --headless \
+  -c "lua require('alex.packer')" \
+  -c "PackerCompile" \
+  -c "PackerInstall" \
+  -c "qa"
 
 # Sync the packer packages
-# nvim --headless "+source ~/.config/nvim/lua/alex/packer.lua" "+PackerSync" "+qa"
+#nvim --headless "+source ~/.config/nvim/lua/alex/packer.lua" "+PackerSync" "+qa"
